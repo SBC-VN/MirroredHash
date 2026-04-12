34b00f57ddf410cf32164dd3a9834db55e357a3f
V <vnestohr@juno.com>
2026-04-12 16:29:41 -0400

    Modified package to use a proxy - so its more 'drop in' replacement for a normal hashtable.

--- src/mirroredhash.cjs
+++ src/mirroredhash.cjs
@@ -2,8 +2,10 @@ const fs = require('fs');
 const CryptoJS = require("crypto-js");
 const path = require('node:path');
 
-function getMirroredHash(options) {
+// Using 'proxy' around a hashtable
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
 
+function getMirroredHash(options) {
     if (!options.hasOwnProperty('filepath')) {
         throw new Error("getMirroredHash: options missing filepath");
     }
@@ -24,77 +26,100 @@ function getMirroredHash(options) {
         rootpath = path.dirname(options.filepath);
     }
     else {
+        // No filename was given, default to "mhash.dat"
         filename = "mhash.dat";
         rootpath = options.filepath;
     }
 
+    // Now we know the root path and filename, we can create them if they don't already exist.
     if (!fs.existsSync(rootpath)) {
         fs.mkdirSync(rootpath, {recursive:true});
     }
 
     let hashfile = rootpath + path.sep + filename;
-    
-    let data = null;
-    if (fs.existsSync(hashfile)) {
-        let filedata = fs.readFileSync(hashfile);
-        data = JSON.parse(filedata);
-    }
-    else {
-        data = {};
-    }
-  
-    let hashObject = {
-        "file": hashfile,
-        "data": data,
-        "paddedkey": null,
-        "paddedvector": null,
-        "paddedhashkey": null,
-        "save": function() {
-                fs.writeFileSync(this.file, JSON.stringify(data,null,2));
-            },
-        "set": function(key, value) {
-                let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkey, {iv: this.paddedvector}).toString() : key;
-                let eValue = this.paddedhashkey ? CryptoJS.AES.encrypt(value, this.paddedhashkey, {iv: this.paddedvector}).toString(): value;
-                this.data[eKey] = eValue;
-                this.save();
-            },
-        "remove": function(key) {
-                let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkey, {iv: this.paddedvector}).toString() : key;
-                if (data.hasOwnProperty(eKey)) {
-                    delete data[eKey];
-                    this.save();
-                }
-            },
-        "get": function(key) {
-                let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkey, {iv: this.paddedvector}).toString() : key;
-                if (data.hasOwnProperty(eKey)) {
-                    let value = data[eKey];
-                    if (this.paddedhashkey) {
-                        let bytes = CryptoJS.AES.decrypt(value, this.paddedhashkey, {iv: this.paddedvector});
-                        value = bytes.toString(CryptoJS.enc.Utf8);
-                    }
-                    return value;
-                }
-                else {
-                    return null;
-                }
-            }
-    }
 
+    // Next, let's encode the hash keys for the hashtable values and.... well.. hashtable keys.  Yeah, different
+    // meanings of 'key' confuse things a bit.  But hey, english.
+    
     // Convert the given hash and vector into 32 character strings.
+    let paddedvaluekey = null;
+    let paddedkeykey = null;       // Cue music to 'can can'
+    let paddedvector = null;
+
     if (options.hasOwnProperty('valuehash')) {
-        hashObject.paddedhashkey = CryptoJS.enc.Hex.parse(options.valuehash.padEnd(32,"5").substring(0,32));
+        paddedvaluekey = CryptoJS.enc.Hex.parse(options.valuehash.padEnd(32,"5").substring(0,32));
     }
 
     if (options.hasOwnProperty('keyhash')) {
-        hashObject.paddedkey = CryptoJS.enc.Hex.parse(options.keyhash.padEnd(32,"5").substring(0,32));
+        paddedkeykey = CryptoJS.enc.Hex.parse(options.keyhash.padEnd(32,"5").substring(0,32));
     }
 
     if (options.hasOwnProperty('vector')) {
-        hashObject.paddedvector = CryptoJS.enc.Hex.parse(options.vector.padEnd(32,"5").substring(0,32));
+        paddedvector = CryptoJS.enc.Hex.parse(options.vector.padEnd(32,"5").substring(0,32));
     }
+    
+    // Set / Read the data for the hashtable, if any.
+    let data = {};
+    if (options.hasOwnProperty('data') && (options.hasOwnProperty('overwiteFile') || !fs.existsSync(hashfile))) {
+        // We've been given data to use and been told to overwrite any file -or- there is no file.
+        for (let [key, value] of Object.entries(options.data)) {
+            let eKey = paddedvaluekey ? CryptoJS.AES.encrypt(key, paddedvaluekey, {iv: this.paddedvector}).toString() : key;
+            let eValue = paddedvaluekey ? CryptoJS.AES.encrypt(value, paddedvaluekey, {iv: this.paddedvector}).toString(): value;
+            data[eKey] = eValue;
+        }
+    }
+    else if (fs.existsSync(hashfile)) {
+        // We have a file.  But if we have data too that could be a problem.
+        if (options.hasOwnProperty('data') && !options.hasOwnProperty('overwriteData')) {
+            // We want to make sure the caller explicitly specifies that we should use the file even if data is specified.
+            throw new Error('getMirroredHash: data given in options AND file already exists.  There can be only one!')
+        }
+        let filedata = fs.readFileSync(hashfile);
+        data = JSON.parse(filedata);
+    }
+   
+    const handler = {
+        "file": hashfile,
+        "paddedkeykey": paddedkeykey,
+        "paddedvaluekey": paddedvaluekey,
+        "paddedvector": paddedvector,
+        save() {
+            fs.writeFileSync(this.file, JSON.stringify(data,null,2));
+        },
+        // Intercepts reading a value
+        get(target, key) {
+            let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkeykey, {iv: this.paddedvector}).toString(): key;
+            if (target.hasOwnProperty(eKey)) {
+                return this.paddedhashkey ? CryptoJS.AES.decrypt(target[eKey], this.paddedvaluekey, {iv: this.paddedvector}).toString(): target[eKey];
+            }
+            return null;
+        },
+        // Intercepts setting a value
+        set(target, key, value) {
+            let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkeykey, {iv: this.paddedvector}).toString(): key;
+            let eValue = this.paddedhashkey ? CryptoJS.AES.encrypt(value, this.paddedvaluekey, {iv: this.paddedvector}).toString(): value;
+            target[eKey] = eValue;
+            this.save();
+            return true; // Success
+        },
+        ownKeys(target) {
+            return Object.keys(target).map(key => {
+                return this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkeykey, {iv: this.paddedvector}).toString(): key;
+            });
+        },
+        deleteProperty(target, key) {
+            let eKey = this.paddedkey ? CryptoJS.AES.encrypt(key, this.paddedkeykey, {iv: this.paddedvector}).toString(): key;
+            if (target.hasOwnProperty(eKey)) {
+                delete target[eKey];
+                this.save();
+            }
+        },
+    };
 
-    return hashObject;
+
+    //return new Proxy(data, traphandler);
+    let proxy = new Proxy(data, handler);
+    return proxy;
 }
 
 module.exports = { getMirroredHash }
--- src/mirroredhash.test.cjs
+++ src/mirroredhash.test.cjs
@@ -2,10 +2,8 @@ const fs = require('fs');
 const { getMirroredHash } = require("./mirroredhash.cjs");
 
 test("Missing filepath", () => {
-  console.log("create without filepath");
   expect(() => getMirroredHash({valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector: "xdaqedf"}).toThrow('getMirroredHash: options missing filepath'))
 });
-
 
 test("Missing valuehash", () => {
   expect(() => getMirroredHash({filepath:"c:/temp/mhash", keyhash:"xxseaerarf", vector:"xyatqdw"}).toThrow('getMirroredHash: options missing valuehash'))
@@ -15,98 +13,139 @@ test("Missing vector", () => {
   expect(() => getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf"}).toThrow('getMirroredHash: options missing valuehash'))
 });
 
-test("Unhashed", () => {
-  let mhash = getMirroredHash({filepath:"c:/temp/mhash"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
+test("File and data", () => {
+  let data = {"one":"first data","two":"second data"};
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
+  let mhash = getMirroredHash({filepath:"c:/temp/mhash/mhash.dat",data:data});
+  expect (() => getMirroredHash({filepath:"c:/temp/mhash/mhash.dat",data:data}).toThrow('getMirroredHash: data given in options AND file already exists.  There can be only one!'))
+});
+
+test("Create parent directory", () => {
+  if (fs.existsSync("c:/temp/mhashcreate")) {
+    fs.rmSync("c:/temp/mhashcreate", {force:true, recursive:true});
+  }
+  let mhash = getMirroredHash({filepath:"c:/temp/mhashcreate"});
+  mhash["one"] = "first value";
+  let result = mhash["one"];
   expect(result).toEqual("first value");
 });
 
+
+test("Unhashed", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
+  let mhash = getMirroredHash({filepath:"c:/temp/mhash"});
+  mhash["one"] = "first value";
+  let result = mhash["one"];
+  expect(result).toEqual("first value");
+});
+
+
 test("Value Hashed", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  let result = mhash["one"];
   expect(result).toEqual("first value");
 });
 
 test("Key Hashed", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", keyhash:"xxxysaaerf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  let result = mhash["one"];
   expect(result).toEqual("first value");
 });
 
-
-test("One Hash", () => {
-  let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
-  expect(result).toEqual("first value");
-});
-
-test("No File", () => {
-  if (fs.existsSync("c:/temp/mhash")) {
-    fs.rmSync("c:/temp/mhash", {recursive:true, force:true});
+test("Fully Hashed", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
   }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  let result = mhash["one"];
   expect(result).toEqual("first value");
 });
 
-
-test("Two Hash", () => {
+test("Get Second value", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  mhash.set("two","second value");
-  mhash.set("three","third value");
-  mhash.set("four","fourth value");
-  result = mhash.get("three");
-  expect(result).toEqual("third value");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  mhash["three"] = "third value";
+  mhash["four"] = "fourth value";
+  let result = mhash["two"];
+  expect(result).toEqual("second value");
 });
 
-test("Delete test", () => {
+test("Delete", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  mhash.set("two","second value");
-  mhash.set("three","third value");
-  mhash.set("four","fourth value");
-  result = mhash.remove("three");
-  result = mhash.get("three");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  mhash["three"] = "third value";
+  mhash["four"] = "fourth value";
+  delete mhash["two"];
+  let result = mhash["two"];
   expect(result).toBeNull();
 });
 
-test("ReRead test (removed)", () => {
+test ("Delete, Reread, then get null", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  mhash.set("two","second value");
-  mhash.set("three","third value");
-  mhash.set("four","fourth value");
-  result = mhash.remove("three");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  mhash["three"] = "third value";
+  mhash["four"] = "fourth value";
+  delete mhash["two"];
+  mhash = null;
   mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  result = mhash.get("three");
+  let result = mhash["two"];
   expect(result).toBeNull();
 });
 
-test("ReRead test (present)", () => {
+
+test ("Delete, Reread, then get key", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
+  }
   let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  mhash.set("two","second value");
-  mhash.set("three","third value");
-  mhash.set("four","fourth value");
-  result = mhash.remove("three");
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  mhash["three"] = "third value";
+  mhash["four"] = "fourth value";
+  mhash = null;
   mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  result = mhash.get("four");
-  expect(result).toEqual("fourth value");
+  let result = mhash["two"];
+  expect(result).toEqual("second value");
 });
 
 
-test("Create directory", () => {
-  if (fs.existsSync("c:/temp/testdir")) {
-    fs.rmSync("c:/temp/testdir", {recursive:true, force:true});
+test ("Get Keys", () => {
+  if (fs.existsSync("c:/temp/mhash/mhash.dat")) {
+    fs.rmSync("c:/temp/mhash/mhash.dat", {force:true});
   }
-  let mhash = getMirroredHash({filepath:"c:/temp/testdir/myfile.dat", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
-  mhash.set("one","first value");
-  result = mhash.get("one");
-  expect(result).toEqual("first value");
+  let mhash = getMirroredHash({filepath:"c:/temp/mhash", valuehash:"xxxysaaerf", keyhash:"xxseaerarf", vector:"xyatqdw"});
+  mhash["one"] = "first value";
+  mhash["two"] = "second value";
+  mhash["three"] = "third value";
+  mhash["four"] = "fourth value";
+  let result = Object.keys(mhash);
+  expect(result).toEqual(["one", "two", "three", "four"]);
 });
